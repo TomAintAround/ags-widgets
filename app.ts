e99d31e81714d083b32792c7d48231e0484715d3
TomAintAround <tcorreia2005@proton.me>
2025-08-06 03:55:48 +0100

    chore: prepare for furture widgets

--- app.ts
+++ app.ts
@@ -2,9 +2,11 @@ import app from "ags/gtk4/app"
 import style from "./style.scss"
 import Bar from "./widgets/Bar"
 
+const widgets = [Bar]
+
 app.start({
 	css: style,
 	main() {
-		app.get_monitors().map(Bar)
+		widgets.map((widget) => app.get_monitors().map(widget))
 	},
 })
